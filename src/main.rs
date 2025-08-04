d61039f598c3f48dbfc91ea6d8d83bd7afca9176
Nino <nino.vanderlaan@hotmail.com>
2025-08-04 18:02:22 +0200

    add filters

--- src/main.rs
+++ src/main.rs
@@ -29,20 +29,109 @@ async fn list_species(
     pool: web::Data<PgPool>,
     query: web::Query<HashMap<String, String>>,
 ) -> impl Responder {
+    let wkt_opt = query.get("wkt");
+    let sci_name_opt = query.get("scientific_name");
+
+    let result = match (wkt_opt, sci_name_opt) {
+        (Some(wkt), Some(sci_name)) => {
+            sqlx::query_as!(
+                Species,
+                r#"
+                SELECT s.id,
+                       COALESCE(t.name_en, s.name) AS name,
+                       s.scientific_name,
+                       s.observed_at,
+                       s.latitude,
+                       s.longitude
+                FROM species s
+                LEFT JOIN species_translations t ON s.scientific_name = t.scientific_name
+                WHERE ST_Contains(ST_GeomFromText($1, 4326), s.geom)
+                  AND s.scientific_name = $2
+                ORDER BY s.observed_at DESC
+                "#,
+                wkt,
+                sci_name
+            )
+            .fetch_all(pool.get_ref())
+            .await
+        }
+        (Some(wkt), None) => {
+            sqlx::query_as!(
+                Species,
+                r#"
+                SELECT s.id,
+                       COALESCE(t.name_en, s.name) AS name,
+                       s.scientific_name,
+                       s.observed_at,
+                       s.latitude,
+                       s.longitude
+                FROM species s
+                LEFT JOIN species_translations t ON s.scientific_name = t.scientific_name
+                WHERE ST_Contains(ST_GeomFromText($1, 4326), s.geom)
+                ORDER BY s.observed_at DESC
+                "#,
+                wkt
+            )
+            .fetch_all(pool.get_ref())
+            .await
+        }
+        (None, Some(sci_name)) => {
+            sqlx::query_as!(
+                Species,
+                r#"
+                SELECT s.id,
+                       COALESCE(t.name_en, s.name) AS name,
+                       s.scientific_name,
+                       s.observed_at,
+                       s.latitude,
+                       s.longitude
+                FROM species s
+                LEFT JOIN species_translations t ON s.scientific_name = t.scientific_name
+                WHERE s.scientific_name = $1
+                ORDER BY s.observed_at DESC
+                "#,
+                sci_name
+            )
+            .fetch_all(pool.get_ref())
+            .await
+        }
+        (None, None) => {
+            sqlx::query_as!(
+                Species,
+                r#"
+                SELECT s.id,
+                       COALESCE(t.name_en, s.name) AS name,
+                       s.scientific_name,
+                       s.observed_at,
+                       s.latitude,
+                       s.longitude
+                FROM species s
+                LEFT JOIN species_translations t ON s.scientific_name = t.scientific_name
+                ORDER BY s.observed_at DESC
+                "#
+            )
+            .fetch_all(pool.get_ref())
+            .await
+        }
+    };
+
+    match result {
+        Ok(species_list) => HttpResponse::Ok().json(species_list),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[get("/species/names")]
+async fn list_species_names(
+    pool: web::Data<PgPool>,
+    query: web::Query<HashMap<String, String>>,
+) -> impl Responder {
     if let Some(wkt) = query.get("wkt") {
         let result = sqlx::query_as!(
-            Species,
+            SpeciesNames,
             r#"
-            SELECT s.id,
-                   COALESCE(t.name_en, s.name) AS name,
-                   s.scientific_name,
-                   s.observed_at,
-                   s.latitude,
-                   s.longitude
-            FROM species s
-            LEFT JOIN species_translations t ON s.scientific_name = t.scientific_name
-            WHERE ST_Contains(ST_GeomFromText($1, 4326), s.geom)
-            ORDER BY s.observed_at DESC
+            SELECT DISTINCT scientific_name FROM species
+            WHERE ST_Contains(ST_GeomFromText($1, 4326), geom);
             "#,
             wkt
         )
@@ -54,33 +143,6 @@ async fn list_species(
             Err(_) => HttpResponse::InternalServerError().finish(),
         };
     }
-
-    let result = sqlx::query_as!(
-        Species,
-        r#"
-        SELECT s.id,
-               COALESCE(t.name_en, s.name) AS name,
-               s.scientific_name,
-               s.observed_at,
-               s.latitude,
-               s.longitude
-        FROM species s
-        LEFT JOIN species_translations t ON s.scientific_name = t.scientific_name
-        "#
-    )
-    .fetch_all(pool.get_ref())
-    .await;
-
-    match result {
-        Ok(species_list) => HttpResponse::Ok().json(species_list),
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    }
-}
-
-#[get("/species/names")]
-async fn list_species_names(
-    pool: web::Data<PgPool>,
-) -> impl Responder {
     let result = sqlx::query_as!(
         SpeciesNames,
         r#"
